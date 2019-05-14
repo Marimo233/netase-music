847ede8fb012777cfd31a6f3318b8d0009aa4814
FRMacPro002 <frmacpro002@wangzhendeMacBook-Pro.local>
2019-05-14 18:32:29 +0800

    初始化

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import  { Switch,Route,Redirect} from 'react-router-dom'
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Find from './pages/Find'
+import MusicHall from './pages/MusicHall'
+import Mine from './pages/Mine'
+import Recommand from './pages/Recommand'
+import TabBar from './components/TabBar'
+import Header from './components/Header'
+import Main from './pages'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div style={{display:"flex",height:500}}>
+      <Header></Header>
+        <Main>
+        <Switch>
+            <Route 
+              exact
+              path='/'
+              render={()=><Redirect to='/MusicHall'/>}
+            />
+            <Route path='/MusicHall' component={MusicHall} />
+            <Route path='/Find' component={Find} />
+            <Route path='/Mine' component={Mine} />
+            <Route path='/Recommand' component={Recommand} />
+          </Switch>
+        </Main>
+        <TabBar></TabBar>
+      </div>
+    )
+  }
 }
-
-export default App;
