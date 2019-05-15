6df1b6799c1318974dc456d6d9863e9eb36248b4
Marimo233 <1072452597@qq.com>
2019-05-15 19:56:48 +0800

    fix

--- src/components/TabBar/index.tsx
+++ src/components/TabBar/index.tsx
@@ -1,10 +1,19 @@
+
+
 import React, { Component } from 'react'
+
+
 
 export default class index extends Component {
   render() {
     return (
       <div>
-        <div className="icon"></div>
+        <div className="icon">
+          <div className="detail"><i className='iconfont icon-yinyue'></i>音乐馆</div>
+          <div className="detail"><i className='iconfont icon-taocantuijian"'></i>推荐</div>
+          <div className="detail"><i className='iconfont icon-faxian'></i>发现</div>
+          <div className="detail"><i className='iconfont icon-wode'></i>我的</div>
+        </div>
       </div>
     )
   }
